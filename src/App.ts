754d0edd44d79de8141c4f383aaf6c8db71893c5
엘풍 <eruhoon@gmail.com>
2024-01-14 13:53:34 +0900

    remove legacy

--- src/App.ts
+++ src/App.ts
@@ -1,10 +1,8 @@
 import { Config } from './Config';
 import { GfDollCacheLoader } from './manager/GfDollCacheLoader';
 import { AfreecaStreamLoader } from './manager/loader/afreeca/AfreecaStreamLoader';
-import { AfreecaUserLoader } from './manager/loader/afreeca/AfreecaUserLoader';
 import { AzurLaneCacheLoader } from './manager/loader/AzurLaneCacheLoader';
 import { CqWarriorCacheLoader } from './manager/loader/CqWarriorCacheLoader';
-import { HentakuLoader } from './manager/loader/HentakuLoader';
 import { HosHeroCacheLoader } from './manager/loader/HosHeroCacheLoader';
 import { KakaoBookLoader } from './manager/loader/KakaoBookLoader';
 import { NaverMovieLoader } from './manager/loader/NaverMovieLoader';
@@ -21,7 +19,6 @@ export class App {
         const SERVER_PORT2 = 9011;
 
         const animationLoader = new OnnadaAnimationLoader();
-        const avActressLoader = new HentakuLoader();
         const alShipLoader = AzurLaneCacheLoader.getInstance();
         const cqWarriorLoader = CqWarriorCacheLoader.getInstance();
         const gfDollCacheLoader = GfDollCacheLoader.getInstance();
@@ -37,7 +34,6 @@ export class App {
         serverManager.setIndexUri(`${Config.ROOT_DIR}/resource/index.html`);
         serverManager.setRiotApiCode(Config.RIOT_API_CODE);
         serverManager.setAnimationLoader(animationLoader);
-        serverManager.setAvActressLoader(avActressLoader);
         serverManager.setAlShipLoader(alShipLoader);
         serverManager.setBookLoader(bookLoader);
         serverManager.setCqWarriorLoader(cqWarriorLoader);
